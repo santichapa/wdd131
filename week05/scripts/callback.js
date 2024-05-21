a80697e018b3d171cb99aeae6068fad8f29f145f
santichapa <santi.chaparro280@gmail.com>
2024-05-21 16:59:54 -0300

    finished callback functions activity

---
+++ week05/scripts/callback.js
@@ -0,0 +1,14 @@
+function calculate(a, b, callback) {
+    callback(a + b);
+  }
+  
+  function displayResult(result) {
+    console.log('The result is: ' + result);
+  }
+
+//   Call a function named calculate and pass it the arguments to 
+//   support the console output of the following equation: 2 + 3. 
+//   You need to pass three arguments to the calculate function.
+
+calculate(2, 3, displayResult);
+
